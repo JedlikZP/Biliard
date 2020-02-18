7e3177af538cf754e46f4cc1ba0ff3f136f6f901
Zalka.Péter <Zalka.Pter@C41-15>
2020-02-18 13:30:04 +0100

    Kezdés eljárás elkészült.

--- Biliard/Program.cs
+++ Biliard/Program.cs
@@ -10,7 +10,23 @@ namespace Biliard
     {
         static void Main(string[] args)
         {
+            List<int> golyók=new List<int>();
+            Kezdés(golyók);
+
+            for (int i = 0; i < golyók.Count; i++)
+            {
+                Console.WriteLine(golyók[i]);
+            }
+
             Console.ReadKey();
+        }
+
+        static void Kezdés(List<int> golyók)
+        {
+            for (int i = 1; i <= 15; i++)
+            {
+                golyók.Add(i);
+            }
         }
     }
 }
